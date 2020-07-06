b6b31e5cbf0434c6da2cae91addf8f592571c0c2
Jess <jessbennett924@gmail.com>
2020-07-05 20:47:05 -0400

    tetsing out sidebar from MUI

---
+++ src/stories/Sidebar.stories.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import Sidebar from '../components/Sidebar/Sidebar';
+
+storiesOf('Sidebar', module).add('first component', () => <Sidebar />);
