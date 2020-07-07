466ebed7603e7e3a1da1fe507807b28fb70cd2a3
Jess <jessbennett924@gmail.com>
2020-07-07 14:45:46 -0400

    updated AppBar aka now called Navigation

--- src/stories/AppBar.stories.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import AppBar from '../components/AppBar/AppBar';
-
-storiesOf('AppBar', module).add('How does this look', () => <AppBar />);
