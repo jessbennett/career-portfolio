5b6716f4273b57c2c1aad162fe035bd6690cb38e
Jess <jessbennett924@gmail.com>
2020-07-07 14:46:07 -0400

    updated app

--- src/App.js
+++ src/App.js
@@ -2,21 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Sidebar from './components/Sidebar/Sidebar';
+// import Sidebar from './components/Sidebar/Sidebar';
 import About from './components/About/About';
+import Navigation from './components/Navigation/Navigation';
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div>
-          <CssBaseline />
-
-          {/* <Route exact path='/' component={LinkList} /> */}
-          <Route exact path='/' component={Sidebar} />
-          <Route exact path='/About' component={About} />
-          {/* <Route exact path='/search' component={Search} /> */}
-        </div>
+        <Switch>
+          <div>
+            <CssBaseline />
+            <Navigation>
+              <Route exact path='/About' component={About} />
+            </Navigation>
+          </div>
+        </Switch>
       </Router>
     );
   }
